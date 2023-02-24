2347dad5285b0205700b1d6b8a76ce8c5d3ecb6f
softyanija <amakou0614@gmail.com>
2023-02-24 18:30:39 +0900

    comentout py-autopep8 temporarily

--- init.el
+++ init.el
@@ -115,9 +115,9 @@
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-irony)) ; backend追加
 
-(require 'py-autopep8)
+;(require 'py-autopep8)
 (define-key python-mode-map (kbd "C-c F") 'py-autopep8) ; バッファ全体のコード整形
-(define-key python-mode-map (kbd "C-c f") 'py-autopep8-region) ; 選択リジョン内のコード整形
+;(define-key python-mode-map (kbd "C-c f") 'py-autopep8-region) ; 選択リジョン内のコード整形
 
 ;; 保存時にバッファ全体を自動整形する
 ;;(add-hook 'before-save-hook 'py-autopep8-before-save)
